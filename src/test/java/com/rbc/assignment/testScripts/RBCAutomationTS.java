72493d2c3a728e876d2b3d55529721eedac40f4e
ashimnanda19 <ashimnanda19@gmail.com>
2018-09-25 05:11:59 -0400

    Updating TestScript as per Modified framework

--- src/test/java/com/rbc/assignment/testScripts/RBCAutomationTS.java
+++ src/test/java/com/rbc/assignment/testScripts/RBCAutomationTS.java
@@ -11,13 +11,14 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
-import com.rbc.assignment.pages.AddToCartPage;
-import com.rbc.assignment.pages.NavBarAmazonHomePage;
-import com.rbc.assignment.pages.SignInPage;
+import com.rbc.assignment.stepsfiles.AddToCartSteps;
+import com.rbc.assignment.stepsfiles.NavBarAmazonHomeSteps;
+import com.rbc.assignment.stepsfiles.SignInSteps;
 import com.rbc.assignment.utility.framework.BaseTestScript;
+
 /***
  * This is a test script class for RBC Automation assignment.
+ * 
  * @author ashimnanda19
  *
  */
@@ -31,7 +32,9 @@ public class RBCAutomationTS extends BaseTestScript {
 	public void setUp(@Optional("Chrome") String browser) {
 		setDriver(browser);
 		driver = getDriver();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.ca/");
+		driver.manage().window().maximize();
 	}
 
 	@AfterTest
@@ -41,27 +44,15 @@ public class RBCAutomationTS extends BaseTestScript {
 
 	@Test
 	public void validateSignInAtCheckout() {
-		driver.get("https://www.amazon.ca/");
-		driver.manage().window().maximize();
-		seHoverOnElement(NavBarAmazonHomePage.get().btnNavbarShopByDepartment, "btnNavbarShopByDepartment");
-		seWaitForClickableWebElement(driver, NavBarAmazonHomePage.get().departmentKindle, 20);
-		seHoverOnElement(NavBarAmazonHomePage.get().departmentKindle, "departmentKindle");
-		seWaitForClickableWebElement(driver, NavBarAmazonHomePage.get().linkKindleOptions_KindlePaperWhite, 20);
-		seClickOnElement(NavBarAmazonHomePage.get().linkKindleOptions_KindlePaperWhite, "linkKindleOptions_KindlePaperWhite");
-		seWaitForClickableWebElement(driver, AddToCartPage.get().ddQuantity, 20);
-		seSelectDropdownByValue(AddToCartPage.get().ddQuantity, "ddQuantity", "2");
-		seClickOnElement(AddToCartPage.get().btnAddToCart, "btnAddToCart");
-		seWaitTillVisiblityOfElement(driver, AddToCartPage.get().btnCloseAddToYourOrderPopUp, 20);
-		seClickOnElement(AddToCartPage.get().btnCloseAddToYourOrderPopUp, "btnProceedToCheckout");
-		seWaitForClickableWebElement(driver,AddToCartPage.get().ConfirmationMessage, 20);
-		seCompareText(AddToCartPage.get().ConfirmationMessage, "ConfirmationMessage", "2 items added to Cart");
-		seClickOnElement(AddToCartPage.get().btnProceedToCheckout, "btnProceedToCheckout");
-		seWaitForClickableWebElement(driver, SignInPage.get().txtEmailOrMobilePhoneNumber, 20);
-		
-		/*Verifying that Email id or Mobile Phone is being asked at the end of scenario*/
-		softAssert.assertEquals(driver.getTitle(),"Amazon Sign In");
-		Assert.assertTrue(SignInPage.get().lblEmailOrMobilePhoneNumber.isDisplayed());
-		Assert.assertTrue(SignInPage.get().txtEmailOrMobilePhoneNumber.isDisplayed());
+		NavBarAmazonHomeSteps.get().selectOptionInDepartment("Kindle", "Kindle Paperwhite");
+		AddToCartSteps.get().selectQuantity("2");
+		AddToCartSteps.get().clickAddToCart();
+		AddToCartSteps.get().closeAddToYourOrderPopUp();
+		softAssert.assertTrue(AddToCartSteps.get().confirmProductAddedToCart(), "Product not added to cart");
+		AddToCartSteps.get().clickCheckoutButton();
+		softAssert.assertTrue(SignInSteps.get().verifyThePageTitle(),"User is not on SignIn page");
+		Assert.assertTrue(SignInSteps.get().verifyPresenceOfEmailandPhoneField());
+
 	}
 
 }
